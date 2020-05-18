6548f1db9616dac4c51c1c2cf6f2a9263978d559
DarioFlores <darioexequiel22@gmail.com>
2020-05-18 10:11:33 -0300

    Segunda clase - Practica

--- app.js
+++ app.js
@@ -4,16 +4,20 @@ const os = require('os');
 const fs = require('fs');
 // console.log(logger);
 
-let pathO = path.parse(__dirname + '/log')
+// let pathO = path.parse(__dirname + '/log')
 
 // console.log(pathO)
-logger.log(`freemen: ${os.freemem()}`)
-logger.log(`totalmen: ${os.totalmem()}`)
-logger.log(`fs: ${fs.readdirSync('./')}`)
+// logger.log(`freemen: ${os.freemem()}`)
+// logger.log(`totalmen: ${os.totalmem()}`)
+// logger.log(`fs: ${fs.readdirSync('./')}`)
 
 
-fs.readdir('./', (err, files) => {
+// fs.readdir('./', (err, files) => {
+    
+    //     (err) ? console.log(err) : console.log(files)
+    // })
 
-    (err) ? console.log(err) : console.log(files)
-})
-
+console.log(logger.getAllFiles('./'))
+logger.printAllFiles('./')
+logger.sortAllFiles('./')
+logger.cantidadFilesLetter('./', 'a')
--- logger.js
+++ logger.js
@@ -1,10 +1,48 @@
+const fs = require('fs');
+
 let url = 'http://myrul.com'
 
+
 log = (mensaje) => {
-    console.log(`Hola ${mensaje}`)
+    console.log(`${mensaje}`)
 }
+
+getAllFiles = (path) => {
+    return fs.readdirSync(path)    
+}
+
+
+printAllFiles = (path) => {
+    fs.readdir(path, (err, files) => {
+        (err) ? console.log(err) : console.log(files)
+    })
+}
+
+sortAllFiles = (path) => {
+    fs.readdir(path, (err, files) => {
+        (err) ? console.log(err) : console.log(files.sort().reverse())
+    })
+}
+
+
+cantidadFilesLetter = (path, letter) => {
+    fs.readdir(path, (err, files) => {
+        if(err){
+            console.log(err)
+        } else {
+            aux = files.filter( word => word.charAt(0) == letter)
+            console.log(aux.length);
+        }
+
+    })
+}
+
 
 module.exports = {
     url,
-    log
+    log,
+    getAllFiles,
+    printAllFiles,
+    sortAllFiles,
+    cantidadFilesLetter
 }
