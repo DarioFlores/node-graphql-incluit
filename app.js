e6f94895477b350476fce5abc1909d280dedafee
DarioFlores <darioexequiel22@gmail.com>
2020-05-18 09:09:31 -0300

    Segunda clase - Teorica

---
+++ app.js
@@ -0,0 +1,19 @@
+const logger = require('./logger')
+const path = require('path');
+const os = require('os');
+const fs = require('fs');
+// console.log(logger);
+
+let pathO = path.parse(__dirname + '/log')
+
+// console.log(pathO)
+logger.log(`freemen: ${os.freemem()}`)
+logger.log(`totalmen: ${os.totalmem()}`)
+logger.log(`fs: ${fs.readdirSync('./')}`)
+
+
+fs.readdir('./', (err, files) => {
+
+    (err) ? console.log(err) : console.log(files)
+})
+
---
+++ logger.js
@@ -0,0 +1,10 @@
+let url = 'http://myrul.com'
+
+log = (mensaje) => {
+    console.log(`Hola ${mensaje}`)
+}
+
+module.exports = {
+    url,
+    log
+}
